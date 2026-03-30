c231efc68513459598b256173e69ac74030a2308
Sammy Irving <samantha.irving@stud.unilu.ch>
2026-03-30 13:30:32 +0200

    created final merged

--- src/constants.py
+++ src/constants.py
@@ -2,5 +2,21 @@ countries = ['ARG', 'AUS', 'BRA', 'CAN', 'CHN', 'FRA', 'DEU', 'IND',
              'IDN', 'ITA', 'JPN', 'MEX', 'RUS', 'SAU', 'ZAF', 'KOR', 
              'TUR', 'GBR', 'USA']
 
-years = list(range(2015, 2025 + 1))
+years = list(range(2015, 2023))
 
+selected_oecd_policies = [
+    'LEV4_CARBONTAX_T',  # Carbon Tax Transport
+    'LEV3_BAN_PHOUT_COAL', # Coal phase-out
+    'LEV4_EE_MANDATE',   # Energy Efficiency Mandates
+    'LEV4_ETS_E_GHG',   # Emissions Trading System (Energy)
+    'LEV4_NZ_DATE' , # Net Zero Target Year
+    'LEV4_CONG_CHARGE', # Congestion Charge
+    'LEV3_NZ', # Net Zero Target
+    'LEV2_CROSS_SEC_CG', #Climate governance
+    'LEV3_CARBONTAX_I', # Carbon Tax Industry
+    'LEV4_BAN_EXTRAC_DATE', # Bans on Fossil Fuel Extraction (with date)
+    'LEV4_FIT_SOL_PR', # Feed-in Tariffs for Solar PV
+    'LEV4_RENEWABLE_EXP', # Planning Renewables Expansion
+    'LEV4_BAN_EXTRAC_DATE', # Bans on Fossil Fuel Extraction (with date)
+    'LEV4_BAN_COAL_STAT', # Ban on the construction of coal power plants - legal status
+]
--- src/data_loader.py
+++ src/data_loader.py
@@ -5,6 +5,8 @@ import glob
 from io import StringIO
 import os
 
+from constants import selected_oecd_policies
+
 # Data loading functions 
 
 #=========================================      World Bank Data    =========================================
@@ -17,22 +19,7 @@ def load_wdi(indicators, countries, start=2015, end=2025):
 
 #=========================================      OECD data   =========================================
 
-selected_oecd_policies = [
-    'LEV4_CARBONTAX_T',  # Carbon Tax Transport
-    'LEV3_BAN_PHOUT_COAL', # Coal phase-out
-    'LEV4_EE_MANDATE',   # Energy Efficiency Mandates
-    'LEV4_ETS_E_GHG',   # Emissions Trading System (Energy)
-    'LEV4_NZ_DATE' , # Net Zero Target Year
-    'LEV4_CONG_CHARGE', # Congestion Charge
-    'LEV3_NZ', # Net Zero Target
-    'LEV2_CROSS_SEC_CG', #Climate governance
-    'LEV3_CARBONTAX_I', # Carbon Tax Industry
-    'LEV4_BAN_EXTRAC_DATE', # Bans on Fossil Fuel Extraction (with date)
-    'LEV4_FIT_SOL_PR', # Feed-in Tariffs for Solar PV
-    'LEV4_RENEWABLE_EXP', # Planning Renewables Expansion
-    'LEV4_BAN_EXTRAC_DATE', # Bans on Fossil Fuel Extraction (with date)
-    'LEV4_BAN_COAL_STAT', # Ban on the construction of coal power plants - legal status
-]
+
 
 def load_oecd_data(url, policy_list=selected_oecd_policies):
     """Fetches OECD data and immediately filters for selected policies."""
